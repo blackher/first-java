6f8ffaac31943d6ab101c2d7fc28a2c1cdc32c82
shihe@wanglibank.com <shihe@wanglibank.com>
2017-08-31 10:18:04 +0800

    s

--- src/main/java/com/test/web/Test.java
+++ src/main/java/com/test/web/Test.java
@@ -1,30 +1,34 @@
 package com.test.web;
 
+import java.util.concurrent.locks.*;
 public class Test {
-    public volatile int inc = 0;
+    public  int inc = 0;
+    Lock lock = new ReentrantLock();
 
-    public void increase() {
-        inc++;
-
+    public  void increase() {
+        lock.lock();
+        try {
+            inc++;
+        } finally{
+            lock.unlock();
+        }
     }
-
 
     public static void main(String[] args) {
         final Test test = new Test();
-        for(int i=0;i<1000;i++){
+        for(int i=0;i<10;i++){
             new Thread(){
                 public void run() {
-                    for(int j=0;j<10;j++)
+                    for(int j=0;j<1000;j++)
                         test.increase();
                 };
             }.start();
-
         }
 
-        if(Thread.activeCount()>=1) { //保证前面的线程都执行完
+        if(Thread.activeCount()>1)  //保证前面的线程都执行完
             Thread.yield();
-        }
         System.out.println(test.inc);
+
 
     }
 }
